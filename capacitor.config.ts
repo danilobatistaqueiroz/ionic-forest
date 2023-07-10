825547c17c5282a96db6d25fecbefb2cd00bf324
danilo batista queiroz <danilobatistaqueiroz@gmail.com>
2023-07-10 02:12:50 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.labs.modalanimations',
+  appName: 'modal-animations',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
